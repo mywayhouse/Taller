4a7437161ba643bc0c5b535a51018231a18d6366
RicardoMatamoros <Zytrux123@gmail.com>
2026-07-20 22:25:02 -0600

    Se agregaron iconos a los KPI´s xd

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -2,25 +2,55 @@
      Vista: Dashboard (Panel Principal)
      ============================================================ -->
 <div class="dashboard-stats">
+
     <div class="stat-card">
-        <h3>Órdenes Pendientes</h3>
-        <p class="stat-number"><?= $ordenes_pendientes ?? 0 ?></p>
+        <div class="icon-stats">
+            <div>
+                <h3>Órdenes Pendientes</h3>
+                <p class="stat-number"><?= $ordenes_pendientes ?? 0 ?></p>
+            </div>
+        <span class="material-icons-outlined" style="font-size: 32px; color: var(--accent-color);">assignment_late</span>
+        </div>
     </div>
+
     <div class="stat-card">
-        <h3>Clientes Registrados</h3>
-        <p class="stat-number"><?= $clientes_activos ?? 0 ?></p>
+        <div class="icon-stats">
+            <div>
+                <h3>Clientes Registrados</h3>
+                <p class="stat-number"><?= $clientes_activos ?? 0 ?></p>
+            </div>
+            <span class="material-icons-outlined" style="font-size: 32px; color: var(--accent-color);">group</span>
+        </div>
     </div>
+
     <div class="stat-card">
-        <h3>Vehículos en Taller</h3>
-        <p class="stat-number"><?= $vehiculos_en_taller ?? 0 ?></p>
+        <div class="icon-stats">
+            <div>
+                <h3>Vehículos en Taller</h3>
+                <p class="stat-number"><?= $vehiculos_en_taller ?? 0 ?></p>
+            </div>
+            <span class="material-icons-outlined" style="font-size: 32px; color: var(--accent-color);">directions_car</span>
+        </div>
     </div>
+
     <div class="stat-card">
-        <h3>Repuestos Stock Bajo</h3>
-        <p class="stat-number"><?= $repuestos_stock_bajo ?? 0 ?></p>
+        <div class="icon-stats">
+            <div>
+                <h3>Repuestos Stock Bajo</h3>
+                <p style="color: red;" class="stat-number"><?= $repuestos_stock_bajo ?? 0 ?></p>
+            </div>
+            <span class="material-icons-outlined" style="font-size: 32px; color: #f16d6d;">inventory_2</span>
+        </div>
     </div>
+
     <div class="stat-card">
-        <h3>Tiempo promedio de pedidos</h3>
-        <p class="stat-number"><?= $tiempo_promedio_pedidos ?? 0 ?></p> <!-- LEVOM: AQIO ESTA ÁRA QIE LLAMES EL PROCEDIMIENTO DEL PROMEDIO DE TIEMPO DE FINALIZACION DE PEDIDOS, OKEY?-->
+        <div class="icon-stats">
+            <div>
+                <h3>Tiempo promedio de pedidos</h3>
+                <p class="stat-number"><?= $tiempo_promedio_pedidos ?? 0 ?></p> <!-- LEVOM: AQIO ESTA ÁRA QIE LLAMES EL PROCEDIMIENTO DEL PROMEDIO DE TIEMPO DE FINALIZACION DE PEDIDOS, OKEY?-->
+            </div>
+            <span class="material-icons-outlined" style="font-size: 32px; color: var(--accent-color);">schedule</span>
+        </div>
     </div>
 </div>
 <!--CHARTS-->
